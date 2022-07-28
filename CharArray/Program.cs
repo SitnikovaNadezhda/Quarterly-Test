593320fe0327b161b829bc31d059a60ebb7fe227
SitnikovaNadezhda <burikh.nadja@mail.ru>
2022-07-28 18:37:16 +0300

    program chararray

---
+++ CharArray/Program.cs
@@ -0,0 +1,17 @@
+﻿string Text = "Some say the world will end in fire, Some say in ice.From what I’ve tasted of desire I hold with those who favor fire.";
+int numb = 3;
+string[] words = Text.Split(new[] { ' ', ',', '.', ';' });
+string[] CharArray = new string[0];
+foreach (string word in words)
+{
+   if (word.Length <= numb)
+    {
+        Array.Resize(ref CharArray, CharArray.Length + 1) ;
+        CharArray[CharArray.Length - 1] = word;
+    }
+}
+for (int i = 0; i < CharArray.Length; i++)
+{
+    Console.Write(CharArray[i] + " ");
+}
+Console.WriteLine();
